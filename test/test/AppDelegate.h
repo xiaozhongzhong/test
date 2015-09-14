13f694a775260204334b83f2b6ba26b00cf01c0f
admin1 <751232050@qq.com>
2015-09-14 12:01:15 +0800

    test

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by admin1 on 15/9/14.
+//  Copyright (c) 2015年 admin1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by admin1 on 15/9/14.
+//  Copyright (c) 2015年 admin1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
